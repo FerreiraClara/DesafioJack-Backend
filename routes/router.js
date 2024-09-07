d79470757108b06a621ff57fafa2a43be4b45c96
Clara Rocha Ferreira <115419127+FerreiraClara@users.noreply.github.com>
2024-09-07 16:31:22 -0300

    fix: cors
    fix: cors

--- routes/router.js
+++ routes/router.js
@@ -4,16 +4,17 @@ const jwt = require("jsonwebtoken");
 const endpoint = require('../controller/endpoint');
 const authMiddleware = require('../middleware/authMiddleware')(jwt);
 
+router.post('/login', endpoint.login)
+router.post('/register-person', endpoint.registerUser);
+
 router.use(authMiddleware.authentication);
 
-router.post('/register-person', endpoint.registerUser);
-router.post('./create-table', endpoint.createTable)
-router.post('./create-task', endpoint.createTask)
-router.post('./get-task', endpoint.getTask)
-router.post('./edit-task', endpoint.editTask)
-router.post('./get-user', endpoint.getUser)
-router.post('./delete-task', endpoint.deleteTask)
-router.post('./login', endpoint.login)
-router.post('./logout', endpoint.logout)
+router.post('/create-table', endpoint.createTable)
+router.post('/create-task', endpoint.createTask)
+router.post('/get-task', endpoint.getTask)
+router.post('/edit-task', endpoint.editTask)
+router.post('/get-user', endpoint.getUser)
+router.post('/delete-task', endpoint.deleteTask)
+router.post('/logout', endpoint.logout)
 
 module.exports = router;
